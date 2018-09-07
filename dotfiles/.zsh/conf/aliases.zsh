5380d146de2a82a76eadd7166c59a0279569dbe3
Martins Talbergs <animaacija@gmail.com>
2018-09-07 18:31:46 +0300

    Abandon all hope, ye who enter here.

--- dotfiles/.zsh/conf/aliases.zsh
+++ dotfiles/.zsh/conf/aliases.zsh
@@ -65,6 +65,16 @@ alias a='php artisan'
 alias c='composer'
 
 #
+## JAVA VERSION SWITCHER
+#
+alias j12="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
+alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
+alias j10="export JAVA_HOME=`/usr/libexec/java_home -v 10`; java -version"
+alias j9="export JAVA_HOME=`/usr/libexec/java_home -v 9`; java -version"
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+alias j7="export JAVA_HOME=`/usr/libexec/java_home -v 1.7`; java -version"
+
+#
 ## MISC
 #
 alias r='ranger'
